59619591e0d44eaf2c8c6ab45965e45470e76d11
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2025-07-11 00:04:52 +0300

    setup the expo react native with nativewind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-unresolved */
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-start justify-center bg-black px-5">
+        <Text className="font-bold text-start text-white text-4xl">
+          Building Digital{'\n'}Solutions That Drive{'\n'}Impact
+        </Text>
+        <Text className="text-[14px] font-light text-white">Haileamlak Waleligne is a Software Engineer, Tech Entrepreneur, and CEO passionate about building scalable systems, AI-powered tools, and modern web experiences.</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
